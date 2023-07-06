25f9a9fa53c92cadf3711567aac056a597911ed6
saggemode <saggemode@gmail.com>
2023-07-06 18:17:43 +0100

    good

---
+++ components/cart/CartIcon.jsx
@@ -0,0 +1,31 @@
+import React, { useContext, useEffect, useRef, useState } from "react";
+import Link from "next/link";
+
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { Store } from "../../utils/Store";
+
+const Basket = () => {
+  const { state } = useContext(Store);
+  const { cart } = state;
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
+
+  return (
+    <div className="relative">
+      <Link href="/cart" legacyBehavior>
+        <a className="relative flex items-center ltr:md:pl-6 rtl:md:pr-6 rtl:md:border-r-2 rtl:md:border-r-slate-300 ltr:md:border-l-2 ltr:md:border-l-slate-300 z-50">
+          <AiOutlineShoppingCart style={{ fontSize: "1.6rem" }} />
+          {cartItemsCount > 0 && (
+            <span className="absolute -top-3 -right-[0.3rem] rtl:md:right-[1rem]  flex items-center justify-center w-5 h-5 rtl:pt-[0.1rem] rounded-full bg-palette-primary text-[0.75rem] leading-3 text-white shadow-lg">
+              {cartItemsCount}
+            </span>
+          )}
+        </a>
+      </Link>
+    </div>
+  );
+};
+
+export default Basket;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
-import '../styles/globals.css'
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+//import { Provider } from "react-redux";
+import store from "../store/store";
+import { useRouter } from 'next/router';
+import { SessionProvider, useSession } from "next-auth/react";
+import { StoreProvider } from "../utils/Store";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <StoreProvider>
+        <Layout>
+          {Component.auth ? (
+            <Auth adminOnly={Component.auth.adminOnly}>
+              <Component {...pageProps} />
+            </Auth>
+          ) : (
+            <Component {...pageProps} />
+          )}
+        </Layout>
+      </StoreProvider>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+function Auth({ children, adminOnly }) {
+  const router = useRouter();
+  const { status, data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/unauthorized?message=login required");
+    },
+  });
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+  if (adminOnly && !session.user.isAdmin) {
+    router.push("/unauthorized?message=admin login required");
+  }
+
+  return children;
+}
+
+export default MyApp;
